50a6d36cbf7ca1bda4624d8851d24740f43c2c2d
Fredrik Hellman <fredrik.hellman@gmail.com>
2023-09-12 22:18:49 +0200

    Cleanup run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,8 @@
 
 make || exit 1
 
-#qemu-system-i386 -fda image
+cat kernel /dev/stdin | qemu-system-i386 -fda image -serial chardev:char0 -chardev stdio,id=char0 
 
-#qemu-system-i386 -s -S -fda image -serial chardev:io -chardev pipe,id=io,path=io &
-#cat kernel > io.in
 #gdb -ex 'target remote localhost:1234' \
 #    -ex 'set tdesc filename target.xml' \
 #    -ex 'set architecture i8086'
-
-cat kernel /dev/stdin | qemu-system-i386 -fda image -serial chardev:char0 -chardev stdio,id=char0 
